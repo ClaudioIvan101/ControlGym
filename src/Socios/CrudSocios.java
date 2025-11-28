e55610c991563264a814d83758b5ab49c2b96577
guster <gustvernengo@gmail.com>
2025-11-28 18:11:35 -0300

    creacion de todos los metodos de crud socio para agregar,modificar,eliminar y obtener

--- src/Socios/CrudSocios.java
+++ src/Socios/CrudSocios.java
@@ -1,4 +1,37 @@
 package Socios;
 
+import java.util.List;
+
 public class CrudSocios {
+    private final List<Socio> socios;
+    
+    public CrudSocios(List<Socio> socios) {
+        this.socios = socios;
+    }
+
+    public void agregarSocio(Socio socio) {
+        socios.add(socio);
+    }
+
+    public Socio buscarSocio(int dni) {
+        for (Socio s : socios) {
+            if (s.getDni() == dni) {
+                return s;
+            }
+        }
+        return null;
+    }
+
+    public boolean eliminarSocio(int dni) {
+        Socio encontrado = buscarSocio(dni);
+        if (encontrado != null) {
+            socios.remove(encontrado);
+            return true;
+        }
+        return false;
+    }
+
+    public List<Socio> obtenerTodos() {
+        return socios;
+    }
 }
