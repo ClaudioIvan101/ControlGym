1617fd8b222d2e1fe44682a12f405fd933349086
LauOporto <lauti10oporto@gmail.com>
2025-12-01 20:57:28 -0300

    Implementacion de CRUDAsistencia para registrar las asistencias cuando el socio llegue al establecimiento

--- src/Asistencias/CrudAsistencia.java
+++ src/Asistencias/CrudAsistencia.java
@@ -1,4 +1,90 @@
 package Asistencias;
 
+import Asistencias.Asistencia;
+import Membresias.Membresia;
+import Socios.Socio;
+
+import java.lang.reflect.Array;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class CrudAsistencia {
+    private final List<Asistencia> asistencias;
+    private final List<Socio> socios;
+    private final List<Membresia> membresias;
+    private final Scanner sc;
+    private static int idGenerator = 1;
+
+    public CrudAsistencia(List<Asistencia> asistencias, List<Socio> socios, List<Membresia> membresias, Scanner sc) {
+        this.asistencias = asistencias;
+        this.socios = socios;
+        this.membresias = membresias;
+        this.sc = sc;
+    }
+
+    public boolean registrarAsistencia() {
+
+        int id = idGenerator++;
+        Socio socioEncontrado = null;
+
+        while (socioEncontrado == null) {
+
+            System.out.print("Ingrese el DNI del socio (ENTER para cancelar): ");
+            Integer dni = leerEntero();
+
+            if (dni == null) {
+                System.out.println("Operación cancelada. Volviendo al menú...");
+                return false;
+            }
+
+            for (Socio s : socios) {
+                if (s.getDni() == dni) {
+                    socioEncontrado = s;
+                    break;
+                }
+            }
+
+            if (socioEncontrado == null) {
+                System.out.println("Socio no encontrado. Intente nuevamente.\n");
+            }
+        }
+
+        System.out.println("Socio encontrado: " + socioEncontrado.getNombre());
+
+        Membresia membresiaEncontrada = socioEncontrado.getMembresia();
+
+        if (membresiaEncontrada == null) {
+            System.out.println("El socio no tiene membresía asignada. No se puede registrar asistencia.");
+            return false;
+        }
+        LocalDateTime fechaHora = LocalDateTime.now();
+
+        Asistencia asistencia = new Asistencia(
+                id,
+                socioEncontrado,
+                fechaHora,
+                membresiaEncontrada
+        );
+
+        asistencias.add(asistencia);
+
+        System.out.println("Asistencia registrada! Número " + id);
+        return true;
+    }
+    private Integer leerEntero() {
+        String input = sc.nextLine().trim();
+
+        if (input.isEmpty()) {
+            return null;
+        }
+
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return null;
+        }
+    }
 }
