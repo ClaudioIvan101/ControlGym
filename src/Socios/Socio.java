414fe502e5eba2c51724f7fb7258e40c75a3c5bb
guster <gustvernengo@gmail.com>
2025-12-01 21:43:49 -0300

    ..

--- src/Socios/Socio.java
+++ src/Socios/Socio.java
@@ -1,22 +1,29 @@
 package Socios;
 
 import Membresias.Membresia;
+import cuotas.Cuota;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Socio extends Persona{
+    private List<Cuota> cuotas = new ArrayList<>();
+    private LocalDate vencimientoActual;
     private Membresia membresia;
-
-    public Socio(int dni, String nombre, int edad) {
-        super(dni, nombre, edad);
-    }
-
 
     @Override
     public String toString() {
         return getNombre() + " (" + getDni() + ") - Membresía: " + membresia.getNombre();
     }
-    public void setMembresia(Membresia membresia) { this.membresia = membresia; }
 
-    public Membresia getMembresia() { return membresia; }
+
+
+    public Socio(int dni, String nombre, int edad) {
+        super(dni, nombre, edad);
+    }
+
 
     @Override
     public void mostrarInfo() {
@@ -25,4 +32,22 @@ public class Socio extends Persona{
 
     public void setMembresia(Membresia membresia) { this.membresia = membresia; }
     public Membresia getMembresia() { return membresia; }
+
+    public List<Cuota> getCuotas() {
+        return cuotas;
+    }
+    public Cuota getUltimaCuotaPagada() {
+        if (cuotas.isEmpty()) return null;
+
+        return cuotas.stream()
+                .filter(Cuota::isPagado)
+                .max(Comparator.comparing(Cuota::getFechaPago))
+                .orElse(null);
+    }
+
+    public boolean tieneCuotaAlDia() {
+        Cuota ultima = getUltimaCuotaPagada();
+        if (ultima == null) return false;
+        return !ultima.getFechaVencimiento().isBefore(LocalDate.now());
+    }
 }
